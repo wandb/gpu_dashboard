8df762a88aa0eda4f260bc11dad10c18a308fc61
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-02-21 08:17:45 +0900

    add manual execution mode

--- lambda/GpuUsage.py
+++ lambda/GpuUsage.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import json
 
 from func import (
     get_new_runs,
@@ -8,31 +9,35 @@ from func import (
     update_companies_table,
     update_overall_table,
 )
-from utils import set_date
+from utils import NOW_UTC
 
 
-def handler(event, context):
+def handler(event: dict[str, str], context: object) -> None:
     # TODO
     # - ステージング環境作成
     # - 簡易テスト（コード内に追加）
-    # - 定期実行と手動データ取得    
     # - データがない日に対応できているか
     # - リファクタ（日付をdatetime.datetimeに統合）
-    print(event)
-    print(context)
-    if True:
-        target_date = None
-    # for day in range(15, 20):
-    #     target_date = datetime.date(2024, 2, day)
-        target_date, processed_at = set_date(target_date=target_date)
-        new_runs_df = get_new_runs(target_date=target_date, processed_at=processed_at)
-        all_runs_df = update_artifacts(df=new_runs_df, target_date=target_date)
-        remove_latest_tags()
-        companies_daily_df = update_companies_table(
-            df=all_runs_df, target_date=target_date
-        )
-        update_overall_table(df=companies_daily_df, target_date=target_date)
-    return
+    target_date_str = event.get("target_date")
+    if target_date_str is None:
+        target_date = datetime.date.today()
+        processed_at = NOW_UTC + datetime.timedelta(hours=9)
+    else:
+        try:
+            target_date = datetime.datetime.strptime(target_date_str, "%Y-%m-%d").date()
+            processed_at = datetime.datetime.combine(
+                target_date + datetime.timedelta(days=1), datetime.time()
+            )
+        except:
+            return {"statusCode": 200, "body": json.dumps("Invalid date format.")}
+    # target_date, processed_at = set_date(target_date=target_date)
+    print(f"Processing {target_date}")
+    new_runs_df = get_new_runs(target_date=target_date, processed_at=processed_at)
+    all_runs_df = update_artifacts(df=new_runs_df, target_date=target_date)
+    remove_latest_tags()
+    companies_daily_df = update_companies_table(df=all_runs_df, target_date=target_date)
+    update_overall_table(df=companies_daily_df, target_date=target_date)
+    return {"statusCode": 200, "body": json.dumps("Succeeded.")}
 
 
 if __name__ == "__main__":
