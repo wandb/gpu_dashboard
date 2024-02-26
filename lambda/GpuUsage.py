8c21312ef01aaae742f01b3695da1c8a4c495823
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-02-26 23:54:35 +0900

    delete files

--- lambda/GpuUsage.py
+++ lambda/GpuUsage.py
@@ -1,32 +1,40 @@
-"""
-TODO
-- ステージング環境作成
-- 簡易テスト（コード内に追加）
-- データがない日に対応できているか
-"""
-
 import os
+import argparse
 import datetime as dt
 import json
-import sys
+import time
 
-from func import (
-    get_new_runs,
-    update_artifacts,
-    remove_latest_tags,
-    update_companies_table,
-    update_overall_table,
-)
+from easydict import EasyDict
+import polars as pl
+from tqdm import tqdm
+import wandb
+import yaml
+
+from y import pipeline, update_artifacts, update_tables, remove_project_tags
 
 
 def handler(event: dict[str, str], context: object) -> None:
-    ### set WANDB API KEY
+    start_time = time.time()
+    ### Read yaml
+    with open("config.yaml") as y:
+        config = EasyDict(yaml.safe_load(y))
+
+    ### Test mode
+    print(f"Test mode: {config.testmode}")
+
+    ### Set WANDB envirionment
     WANDB_API_KEY = event.get("WANDB_API_KEY")
     if WANDB_API_KEY is not None:
         del os.environ["WANDB_API_KEY"]
         os.environ["WANDB_API_KEY"] = WANDB_API_KEY
+    os.environ["WANDB_CACHE_DIR"] = config.wandb_dir
+    os.environ["WANDB_DATA_DIR"] = config.wandb_dir
+    os.environ["WANDB_DIR"] = config.wandb_dir
+    # Check
+    print(f"Default entity: {wandb.api.default_entity}")
 
-    ### set target date
+    ### Set target date
+    target_date: dt.date
     target_date_str = event.get("target_date")
     if target_date_str is None:
         target_date = dt.date.today()
@@ -34,26 +42,84 @@ def handler(event: dict[str, str], context: object) -> None:
         try:
             target_date = dt.datetime.strptime(target_date_str, "%Y-%m-%d").date()
         except:
-            return {"statusCode": 200, "body": json.dumps("Invalid date format.")}
-    print(f"Processing {target_date} ...")
+            print(body := "!!! Invalid date format !!!")
+            return {"statusCode": 200, "body": json.dumps(body)}
+    # Check
+    print(f"Target date: {target_date}")
 
-    ### update tables
-    ## get runs data
-    new_runs_df = get_new_runs(target_date=target_date)
-    all_runs_df = update_artifacts(df=new_runs_df, target_date=target_date)
-    remove_latest_tags()
-    ## aggregate
-    companies_daily_df = update_companies_table(df=all_runs_df, target_date=target_date)
-    update_overall_table(df=companies_daily_df, target_date=target_date)
-    return {"statusCode": 200, "body": json.dumps("Succeeded.")}
+    # -------------#
+    # Update data #
+    # -------------#
+    ### Get new runs
+    df_list = []
+    company_config: EasyDict
+    for company_config in tqdm(config.companies):
+        if (config.testmode) & (len(df_list) == 2):
+            continue
+        company_runs_df: pl.DataFrame = pipeline(
+            company_name=company_config.company_name,
+            gpu_schedule=company_config.schedule,
+            target_date=target_date,
+            logged_at=dt.datetime.now(),
+            ignore_tag=config.ignore_tag,
+            testmode=config.testmode,
+        )
+        if company_runs_df.is_empty():
+            continue
+        df_list.append(company_runs_df)
+    if not df_list:
+        print("No runs found")
+        new_runs_df = pl.DataFrame()
+    else:
+        new_runs_df = pl.concat(df_list)
+        print(f"{len(new_runs_df)} runs found.")
+
+    ### Remove project tags
+    remove_project_tags(
+        entity=config.path_to_dashboard.entity,
+        project=config.path_to_dashboard.project,
+        delete_tags=config.tag_for_latest,
+        head=(len(config.companies) + 2) * 2, # +2はupdateとreadの分
+    )
+    ### Update artifacts
+    end_time1 = time.time()
+    elapsed_time1 = "{} min {} sec".format(
+        int((end_time1 - start_time) // 60), int((end_time1 - start_time) % 60)
+    )
+    new_records: int = update_artifacts(
+        new_runs_df=new_runs_df,
+        target_date=target_date,
+        wandb_dir=config.wandb_dir,
+        path_to_dashboard=config.path_to_dashboard,
+        elapsed_time=elapsed_time1,
+        testmode=config.testmode,
+    )
+    ### Add summary
+    result = {}
+    result["target_date"] = target_date_str
+    result["new_records"] = new_records
+    result["elapsed_time1"] = elapsed_time1
+    result["testmode"] = config.testmode
+    # --------------#
+    # Update table #
+    # --------------#
+    ### Upate tables
+    update_tables(
+        wandb_dir=config.wandb_dir,
+        companies_config=config.companies,
+        path_to_dashboard=config.path_to_dashboard,
+        target_date=target_date,
+        tag_for_latest=config.tag_for_latest,
+    )
+    return {"statusCode": 200, "body": json.dumps(result)}
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        event = {"WANDB_API_KEY": sys.argv[1]}
-    elif len(sys.argv) == 3:
-        event = {"WANDB_API_KEY": sys.argv[1], "target_date": sys.argv[2]}
-    else:
-        print("WANDB API KEY not provided.")
-        exit()
+    ### Parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api", type=str, required=True)
+    parser.add_argument("--target-date", type=str)
+    args = parser.parse_args()
+    ### Run
+    event = {"WANDB_API_KEY": args.api, "target_date": args.target_date}
     handler(event=event, context=None)
