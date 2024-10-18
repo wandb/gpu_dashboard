2494125d2ca57f1aa97202f1d389c855d44d86cf
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-10-18 13:54:10 +0900

    modify: main実行時の日付の設定を変更

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime as dt
 import os
+import pytz
 
 from src.tracker.run_manager import RunManager
 from src.uploader.run_uploader import RunUploader
@@ -10,21 +11,20 @@ from src.calculator.remove_tags import remove_latest_tags
 from src.calculator.gpu_usage_calculator import GPUUsageCalculator
 
 def validate_dates(start_date, end_date):
-    today = dt.date.today()
-    yesterday = today - dt.timedelta(days=1)
-    default_start = dt.date(2024, 2, 15)
+    default_start = dt.date(2024, 10, 25)
+    default_end = dt.date.today() - dt.timedelta(days=1)
 
     if start_date is None and end_date is None:
         # 両方指定なしの場合、昨日の日付を両方に設定
-        return yesterday.strftime("%Y-%m-%d"), yesterday.strftime("%Y-%m-%d")
+        return default_end.strftime("%Y-%m-%d"), default_end.strftime("%Y-%m-%d")
     elif start_date is None:
-        # start_dateが指定されていない場合、デフォルトを2024-02-15に設定
+        # start_dateが指定されていない場合、デフォルトを2024-10-25に設定
         start = default_start
         end = dt.datetime.strptime(end_date, "%Y-%m-%d").date()
     elif end_date is None:
         # end_dateが指定されていない場合、昨日の日付を設定
         start = dt.datetime.strptime(start_date, "%Y-%m-%d").date()
-        end = yesterday
+        end = default_end
     else:
         # 両方指定ありの場合
         start = dt.datetime.strptime(start_date, "%Y-%m-%d").date()
@@ -37,6 +37,10 @@ def validate_dates(start_date, end_date):
     return start.strftime("%Y-%m-%d"), end.strftime("%Y-%m-%d")
 
 def main():
+    # 現在の日時（日本時間）
+    current_time = dt.datetime.now(pytz.timezone('Asia/Tokyo'))
+    print(f"Current time (JST): {current_time}")
+
     parser = argparse.ArgumentParser(description="Fetch and process run data from Weights & Biases")
     parser.add_argument("--api", type=str, help="Weights & Biases API Key")
     parser.add_argument("--start-date", type=str, help="Start date for data fetch (YYYY-MM-DD)")
