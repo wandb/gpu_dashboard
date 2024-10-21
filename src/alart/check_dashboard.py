b90ffde23fe303e8851a4437a095d459f78d5b6b
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-10-21 14:59:23 +0900

    modify: 進行中でない企業はアラートを出さないように修正

--- src/alart/check_dashboard.py
+++ src/alart/check_dashboard.py
@@ -1,140 +1,129 @@
 from dataclasses import dataclass
 import datetime as dt
+from typing import List, Set
 import pytz
-
 from easydict import EasyDict
 import wandb
 import yaml
 
-
 @dataclass
 class CompanySchedule:
     company: str
-    start_date: dt.datetime
-
+    start_date: dt.date
 
 @dataclass
 class UpdateError:
     title: str
     text: str
 
+class Config:
+    def __init__(self, config_path: str):
+        with open(config_path, "r") as f:
+            self.data = EasyDict(yaml.safe_load(f))
+        self.LOCAL_TZ = pytz.timezone("Asia/Tokyo")
+        self.TARGET_DATE = dt.datetime.now(self.LOCAL_TZ).date() + dt.timedelta(days=-1)
+        self.TARGET_DATE_STR = self.TARGET_DATE.strftime("%Y-%m-%d")
 
-with open("config.yaml", "r") as f:
-    CONFIG = EasyDict(yaml.safe_load(f))
+class DashboardChecker:
+    def __init__(self, config: Config):
+        self.config = config
+        self.api = wandb.Api()
 
-# 今日の日付を取得
-LOCAL_TZ = pytz.timezone("Asia/Tokyo")
-TARGET_DATE = dt.datetime.now(LOCAL_TZ).date() + dt.timedelta(days=-1)
-TARGET_DATE_STR = TARGET_DATE.strftime("%Y-%m-%d")
+    def check_dashboard(self) -> None:
+        """ダッシュボードの健全性をチェックする"""
+        companies = self.get_in_progress_companies()
+        errors = []
 
-
-def check_dashboard() -> None:
-    """run_nameとtagをチェックする"""
-    # 企業名のsetを取得
-    company_schedule = get_company_schedule()
-    companies = in_progress(company_schedule=company_schedule)
-    companies.add("overall")
-    # runを取得
-    runs = get_runs()
-    # チェック
-    check(companies=companies, runs=runs)
-
-    return None
-
-
-def get_company_schedule() -> list[CompanySchedule]:
-    """企業名と開始日を取得する"""
-    companies = []
-    for company in CONFIG.companies:
-        start_date = min(
-            [dt.datetime.strptime(s.date, "%Y-%m-%d").date() for s in company.schedule]
-        )
-        company_schedule = CompanySchedule(
-            company=company.company, start_date=start_date
-        )
-        companies.append(company_schedule)
-
-    return companies
-
-
-def in_progress(company_schedule: list[CompanySchedule]) -> set[str]:
-    """進行中の企業を取得する"""
-    companies = set(
-        c.company for c in company_schedule if TARGET_DATE >= c.start_date
-    )
-
-    return companies
-
-
-def get_runs() -> object:
-    """runを取得する"""
-    api = wandb.Api()
-    project_path = "/".join((CONFIG.dashboard.entity, CONFIG.dashboard.project))
-    runs = api.runs(path=project_path)
-
-    return runs
-
-
-def check(companies: set[str], runs: object) -> None:
-    # エラーを収集
-    update_errors = []
-    companies_found = []
-    tag_for_latest = CONFIG.dashboard.tag_for_latest
-    for run in runs:
-        if tag_for_latest in run.tags:
-            another_tags = [r for r in run.tags if r != tag_for_latest]
-            # latest以外のtagの数が1つであることを確認
-            try:
-                assert len(another_tags) == 1
-                companies_found.append(another_tags[0])
-            except AssertionError:
-                update_errors.append(
-                    UpdateError(title="Error of number of tags", text=str(run.tags))
-                )
-            # 最新のデータが昨日分であることを確認
-            try:
-                target_date_str_found = run.name.split("_")[-1]
-                assert target_date_str_found == TARGET_DATE_STR
-            except AssertionError:
-                update_errors.append(
-                    UpdateError(
-                        title="Error of target date",
-                        text=f"{TARGET_DATE_STR}, {run.name}",
-                    )
-                )
-    # 企業数の一致を確認
-    try:
-        assert len(companies) == len(companies_found)
-    except AssertionError:
-        update_errors.append(
-            UpdateError(title="Error of number of latest runs", text="")
-        )
-    # 企業名の一致を確認
-    try:
-        assert companies == set(companies_found)
-    except AssertionError:
-        update_errors.append(
-            UpdateError(title="Error of companies", text=str(companies_found))
-        )
-    # wandbでアラートを送信
-    with wandb.init(
-        entity=CONFIG.dashboard.entity,
-        project=CONFIG.dashboard.project,
-        name=f"Health Alert",
-    ) as run:
-        alert_title = "Dashboard health check"
-        if update_errors:
-            msg = ""
-            for update_error in update_errors:
-                msg += f"{update_error.title}: {update_error.text}\n"
-            print(msg)
-            wandb.alert(title=alert_title, text=msg)
+        if not companies:
+            errors.append(UpdateError(title="No active companies", text="There are no companies currently active."))
         else:
-            msg = "No errors found"
+            runs = self.get_runs()
+            errors = self.check_runs(companies, runs)
+
+        self.send_alert(errors)
+
+    def get_company_schedule(self) -> List[CompanySchedule]:
+        """企業名と開始日を取得する"""
+        return [
+            CompanySchedule(
+                company=company.company,
+                start_date=min(dt.datetime.strptime(s.date, "%Y-%m-%d").date() for s in company.schedule)
+            )
+            for company in self.config.data.companies
+        ]
+
+    def get_in_progress_companies(self) -> Set[str]:
+        """進行中の企業を取得する"""
+        today = self.config.TARGET_DATE
+        companies = set()
+        
+        for company in self.config.data.companies:
+            start_date = min(dt.datetime.strptime(s['date'], "%Y-%m-%d").date() for s in company.schedule)
+            end_date = max(dt.datetime.strptime(s['date'], "%Y-%m-%d").date() for s in company.schedule)
+            
+            # 開始日以降かつ終了日の前日まで進行中とみなす
+            if start_date <= today < end_date:
+                companies.add(company.company)
+        
+        # 進行中の企業がある場合のみ "overall" を追加
+        if companies:
+            companies.add("overall")
+        return companies
+
+    def get_runs(self) -> object:
+        """runを取得する"""
+        project_path = f"{self.config.data.dashboard.entity}/{self.config.data.dashboard.project}"
+        return self.api.runs(path=project_path)
+
+    def check_runs(self, companies: Set[str], runs: object) -> List[UpdateError]:
+        """runをチェックし、エラーがあれば返す"""
+        errors = []
+        companies_found = []
+        tag_for_latest = self.config.data.dashboard.tag_for_latest
+
+        for run in runs:
+            if tag_for_latest in run.tags:
+                another_tags = [r for r in run.tags if r != tag_for_latest]
+                self.check_tags(another_tags, run.tags, companies_found, errors)
+                self.check_target_date(run.name, errors)
+
+        self.check_companies(companies, companies_found, errors)
+        return errors
+
+    def check_tags(self, another_tags: List[str], all_tags: List[str], companies_found: List[str], errors: List[UpdateError]) -> None:
+        if len(another_tags) != 1:
+            errors.append(UpdateError(title="Error of number of tags", text=str(all_tags)))
+        else:
+            companies_found.append(another_tags[0])
+
+    def check_target_date(self, run_name: str, errors: List[UpdateError]) -> None:
+        target_date_str_found = run_name.split("_")[-1]
+        if target_date_str_found != self.config.TARGET_DATE_STR:
+            errors.append(UpdateError(title="Error of target date", text=f"{self.config.TARGET_DATE_STR}, {run_name}"))
+
+    def check_companies(self, companies: Set[str], companies_found: List[str], errors: List[UpdateError]) -> None:
+        if len(companies) != len(companies_found):
+            errors.append(UpdateError(title="Error of number of latest runs", text=""))
+        if companies != set(companies_found):
+            errors.append(UpdateError(title="Error of companies", text=str(companies_found)))
+
+    def send_alert(self, errors: List[UpdateError]) -> None:
+        """wandbでアラートを送信する"""
+        with wandb.init(
+            entity=self.config.data.dashboard.entity,
+            project=self.config.data.dashboard.project,
+            name="Health Alert",
+        ) as run:
+            alert_title = "Dashboard health check"
+            if errors:
+                msg = "\n".join(f"{error.title}: {error.text}" for error in errors)
+            else:
+                msg = "No errors found. All active companies are reporting as expected."
+            
             print(msg)
             wandb.alert(title=alert_title, text=msg)
-
-    return
 
 if __name__ == "__main__":
-    check_dashboard()
+    config = Config("config.yaml")
+    checker = DashboardChecker(config)
+    checker.check_dashboard()
